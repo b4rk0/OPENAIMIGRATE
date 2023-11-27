866e29c5cfafa0c3124804070b678bad1ca5bc02
Barko <brk0@waroffice.us>
2023-11-27 09:23:47 -0500

    init commit

---
+++ draw-me-a-liger.py
@@ -0,0 +1,18 @@
+import openai
+
+# Set up your OpenAI API key
+openai.api_key = "your-api-key"
+
+# Provide a prompt for image generation
+prompt = "Draw me a liger having tea with a lion and a tiger"
+
+# Use OpenAI's DALL-E to generate an image based on the prompt
+response = openai.Completion.create(
+    engine="image-alpha-001", prompt=prompt, n=1, stop=None
+)
+
+# Extract the generated image URL from the response
+image_url = response["choices"][0]["image"]["url"]
+
+# Display or use the generated image as needed
+print("Generated Image URL:", image_url)
---
+++ generate.py
@@ -0,0 +1,56 @@
+import openai  # OpenAI Python library to make API calls
+import requests  # used to download images
+import os  # used to access filepaths
+from PIL import Image  # used to print and edit images
+
+# set API key
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+# set a directory to save DALL·E images to
+image_dir_name = "images"
+image_dir = os.path.join(os.curdir, image_dir_name)
+
+# create the directory if it doesn't yet exist
+if not os.path.isdir(image_dir):
+    os.mkdir(image_dir)
+
+# print the directory to save to
+print(f"{image_dir=}") 
+
+# create an image
+
+# set the prompt
+# prompt = "A   vfcyberpunk monkey hacker dreaming of a beautiful bunch of bananas, digital art"
+
+prompt = "Draw me a liger having tea with a lion and a tiger"
+# call the OpenAI API
+generation_response = openai.Image.create(
+    prompt=prompt,
+    n=1,
+    size="1024x1024",
+    response_format="url",
+)
+
+# print response
+print(generation_response)
+
+# save the image
+generated_image_name = "generated_image.png"  # any name you like; the filetype should be .png
+generated_image_filepath = os.path.join(image_dir, generated_image_name)
+generated_image_url = generation_response["data"][0]["url"]  # extract image URL from response
+generated_image = requests.get(generated_image_url).content  # download the image
+
+with open(generated_image_filepath, "wb") as image_file:
+    image_file.write(generated_image)  # write the image to the file
+
+
+
+
+# print the image
+print(generated_image_filepath)
+display(Image.open(generated_image_filepath))
+
+
+
+
+
